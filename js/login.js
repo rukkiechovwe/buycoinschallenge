378e9a471335b0eeb72b2b31134e011cc3272ffd
rukkiechovwe <rukkiechowe@gmail.com>
2021-05-29 18:31:48 +0100

    final editing

--- js/login.js
+++ js/login.js
@@ -2,12 +2,9 @@ document.querySelector("#user-form").addEventListener("submit", (e) => {
   e.preventDefault();
   const username = document.querySelector("#user-input").value;
 
-  // (B) URL PARAMETERS
   var params = new URLSearchParams();
   params.append("username", username);
 
-  // (C) GO!
   var url = "index.html?" + params.toString();
   location.href = url;
-  //   history.pushState({"i": "hello world"},"User","https://localhost:")
 });
